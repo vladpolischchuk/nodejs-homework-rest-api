65bd87d0c542e2f4a8b0a99d7e51ef3e1c9a14b1
Владислав Поліщук <105670228+vladpolischchuk@users.noreply.github.com>
2023-05-06 19:12:38 +0300

    hm №5

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const { validateBody } = require("../../utils/index");
 
-const { authenticate } = require("../../middlewares/index");
+const { authenticate, upload } = require("../../middlewares/index");
 
 const { schemas } = require("../../models/user");
 
@@ -19,5 +19,7 @@ router.get("/current", authenticate, ctrl.getCurrent);
 router.post("/logout", authenticate, ctrl.logout);
 
 router.patch("/users", authenticate, validateBody(schemas.updateSubscription), ctrl.updateSubscription)
+
+router.patch("/avatars", authenticate, upload.single("avatar"), ctrl.updateAvatar);
 
 module.exports = router;
